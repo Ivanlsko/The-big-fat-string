89f437e4092a28876084f5d55eb6f26745a8926f
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2021-08-30 20:03:40 +0200

    theOption defined

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let theValue;
+let options = document.querySelectorAll("#options > option");
+let theOption = options[0].value;
 
 document.querySelector("#submitBut").addEventListener("click", (e) => {
   e.preventDefault();
   theValue = document.querySelector("#theInput > #input").value;
+  makeTheMethod();
 });
+
+options.forEach((e) => {
+  e.addEventListener("click", () => {
+    console.log(e);
+    theOption = e.value;
+  });
+});
+
+function makeTheMethod() {
+  console.log("I am connected");
+}
